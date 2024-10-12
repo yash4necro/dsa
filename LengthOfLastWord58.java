d0c53a6a1070a002f456ce061faef5a39271f22b
yash4necro <yash4167@gmail.com>
2024-10-12 12:35:42 +0530

    58_LengthOfLastWord

---
+++ LengthOfLastWord58.java
@@ -0,0 +1,10 @@
+class lengthOfLastWord58 {
+    public int lengthOfLastWord(String s) {
+        String [] arr = s.split(" ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i].length() == 0) continue;
+            else return arr[i].length();
+        }
+        return 0;
+    }
+}
